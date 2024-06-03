7ec160628f00e71feae79a6bb92f8ff46cc29881
acurzon <adam@curzon.org>
2024-06-03 12:57:06 +0100

    feat: setup doctrine

--- dependencies.php
+++ dependencies.php
@@ -2,13 +2,45 @@
 
 namespace App;
 
+use Doctrine\ORM\ORMSetup;
+use Doctrine\ORM\EntityManager;
+use Doctrine\DBAL\DriverManager;
 use App\Controller\CarController;
+use App\Repository\CarRepository;
 use App\Service\FileLoggerService;
 
 $container = new Container();
 
-$container->set('CarController', function () {
-    return new CarController();
+$container->set('EntityManager', function () {
+    $config = ORMSetup::createAttributeMetadataConfiguration(
+        paths: array(__DIR__ . "/src"),
+        isDevMode: true,
+    );
+
+    $connection = DriverManager::getConnection([
+        'driver' => 'pdo_sqlite',
+        'path' => __DIR__ . '/db.sqlite',
+    ], $config);
+
+    return new EntityManager($connection, $config);
+});
+
+
+// Anything registerd to the container under this is not needed in CLI
+if (php_sapi_name() === 'cli') {
+    return;
+}
+
+$container->set('CarRepository', function () use ($container) {
+    return new CarRepository(
+        $container->get("EntityManager")
+    );
+});
+
+$container->set('CarController', function () use ($container) {
+    return new CarController(
+        $container->get("CarRepository")
+    );
 });
 
 $container->set('LoggerService', function () {
