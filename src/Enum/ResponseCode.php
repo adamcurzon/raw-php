dbdc236847c2ba9e31a1d47d5db4ef48061ff185
acurzon <adam@curzon.org>
2024-06-03 13:01:40 +0100

    chore: update response codes

--- src/Enum/ResponseCode.php
+++ src/Enum/ResponseCode.php
@@ -5,7 +5,11 @@ namespace App\Enum;
 enum ResponseCode: int
 {
     case OK = 200;
+    case CREATED = 201;
+
+    case BAD_REQUEST = 400;
     case NOT_FOUND = 404;
     case METHOD_NOT_ALLOWED = 405;
+
     case SERVER_ERROR = 500;
 }
