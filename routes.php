f3dd6f8a5b2d55a6b6d3607c400eb005d28cb1e1
acurzon <adam@curzon.org>
2024-06-03 10:33:17 +0100

    feat: define base routes for car controller

---
+++ routes.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+define('ROUTES', [
+    '/car' => [
+        "GET" => [
+            "controller" => "CarController",
+            "function" => "index"
+        ],
+        "POST" => [
+            "controller" => "CarController",
+            "function" => "create"
+        ]
+    ]
+]);
