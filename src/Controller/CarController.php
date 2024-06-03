0db06df642842f0c63122ff80ed0ecc78c80f757
acurzon <adam@curzon.org>
2024-06-03 13:00:58 +0100

    refactor: use repository in car controller

--- src/Controller/CarController.php
+++ src/Controller/CarController.php
@@ -2,13 +2,32 @@
 
 namespace App\Controller;
 
-use App\Response\Response;
+use App\Model\Car;
+use App\Http\Request;
+use App\Http\Response;
 use App\Enum\ResponseCode;
+use App\Repository\CarRepository;
+use App\Http\Validator\CreateCarValidator;
 
 class CarController
 {
-    public function index(): Response
+    public function __construct(private CarRepository $carRepository)
     {
-        return new Response(ResponseCode::OK, data: ["Fiesta", "BMW"]);
+    }
+
+    public function index(Request $request): Response
+    {
+        return new Response(ResponseCode::OK, data: $this->carRepository->findAll());
+    }
+
+    public function create(Request $request): Response
+    {
+        $request->validateJson(new CreateCarValidator());
+
+        $car = new Car();
+        $car->setName($request->get("name"));
+        $this->carRepository->save($car);
+
+        return new Response(ResponseCode::CREATED, "Car created", $car->toArray());
     }
 }
